c8997d3f34d3ad94655f53b110d52734c987b75e
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 21:26:16 +0700

    js lab 49

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+let user = {};
+
+user.name = "john";
+user.surname = "Do";
+console.log(user);
+
+user['name'] = "Matt";
+console.log(user);
+
+delete user.name;
+console.log(user);
+
+
+
+
